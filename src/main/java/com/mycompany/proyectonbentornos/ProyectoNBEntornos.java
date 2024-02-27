5fe1b6e542fa00394e67a6c794ce5d3be7ff2812
KevinV <kavillarreala@gmail.com>
2024-02-27 21:08:16 +0100

    Iniciar repositorio. Primer commit.

---
+++ src/main/java/com/mycompany/proyectonbentornos/ProyectoNBEntornos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectonbentornos;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class ProyectoNBEntornos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
